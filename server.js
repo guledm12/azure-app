4fe89e8ae201d2ccc261a3a36eb352f82bbd8bd1
guledm12 <gmohamed@kinaxis.com>
2023-01-23 16:25:28 -0500

    added modules and github actions .yml file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var fs = require('fs'); 
 
-fs.readFile('./index.html', function (err, html) {
+fs.readFile('index.html', function (err, html) {
 
     if (err) throw err;    
 
